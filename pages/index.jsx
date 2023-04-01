6eb81c5f79e33927126c37d8ffa0696a5ed40483
Veer Shrivastav <srivastav.veer@gmail.com>
2023-04-01 17:57:32 +0530

    Menu is now mobile compatible

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,21 +1,48 @@
 import Head from 'next/head';
 import Banner from '../page-components/banner';
 import SideProfile from '../page-components/side-profile';
+import { Button } from 'react-bootstrap';
+import {useState} from 'react';
 
 export default function Home() {
+
+	// color: #FDF7FF !important;
+	// border: 2px solid #FDF7FF !important;
+
+	const [btnColorClass, setBtnColorClass] = useState('btnLight');
+	const [closeState, setCloseState] = useState(1);
+	const [menuClass, setMenuClass] = useState('sideMenu');
+	const [btnIcon, setBtnIcon] = useState('bi-grid-fill');
+
+	const toggleMenu = () => {
+		let newState = closeState==1 ? 0 : 1;
+		let newClass = newState==1 ? 'sideMenu' : 'sideMenuOut';
+		if (newState) {
+			setBtnColorClass('btnLight');
+			setBtnIcon('bi-grid-fill');
+		} else {
+			setBtnColorClass('btnDark');
+			setBtnIcon('bi-x');
+		}
+		setCloseState(newState);
+		setMenuClass(newClass);
+	}
 
   	return (
 		<>
 			<Head>
 				<title>Veer Shrivastav</title>
-				<meta name="description" content="Veer Shrivastav is an Entrepreneur, Technologist and Explorer." />
+				<meta name="description" content="Veer Shrivastav is an Entrepreneur, Technologist, Engineering Leader and Developer." />
 				<link rel="icon" href="/favicon.ico" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 			</Head>
 			<main>
 					<div className='container'>
+						<Button className={`menuBtn d-block d-md-none ${btnColorClass}`} onClick={toggleMenu}>
+							<i className={`bi ${btnIcon}`}></i>
+						</Button>
 						<div className='row'>
-							<div className='col-sm-12 col-md-4 col-lg-3 d-none d-md-block'>
+							<div className={`col-sm-12 col-md-4 col-lg-3 ${menuClass}`} style={{'--close': closeState}}>
 								<SideProfile />
 							</div>
 							<div className='col-sm-12 col-md-8 col-lg-9'>
