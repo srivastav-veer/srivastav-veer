a9c8c61116f697c5bd8152b219de66519089a8e1
Veer Shrivastav <srivastav.veer@gmail.com>
2023-04-03 22:09:27 +0530

    Profile complete

---
+++ page-components/profile/data/dist/experience.dev.js
@@ -0,0 +1,82 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.experiences = void 0;
+var experiences = [{
+  organization: {
+    name: 'DaMENSCH',
+    url: 'https://damensch.com'
+  },
+  endTime: 'March 2023',
+  startTime: 'July 2021',
+  position: 'VP of Engineering',
+  skills: ['Node.js', 'MongoDB', 'MySQL', 'PHP', 'Redis', 'Docker', 'Kubernetes', 'AWS', 'PHP', 'ReactJs', 'React-Native'],
+  desciption: "DaMENSCH is India's leading online D2C apparel brand. The organization \n        does a revenue of more than $20M from its website, I joined the team to upgrade \n        the in-house technology implementation. I was responsible for building, and \n        managing the tech team, and setting up the end-to-end technology process.",
+  pointerHeader: 'Key Contributions:',
+  pointers: ['Saved more than $40K on cloud bills by optimizing cloud infra.', 'Set up the first core engineering team (13 Engineers) in less than 2 months.', 'Created the end-to-end sprint process.']
+}, {
+  organization: {
+    name: 'Toppr',
+    url: 'https://www.toppr.com/'
+  },
+  endTime: 'August 2021',
+  startTime: 'Augsut 2020',
+  position: 'Engineering Manager',
+  skills: ['Node.js', 'MongoDB', 'Redis', 'AWS', 'ReactJs', 'React-Native'],
+  desciption: "Toppr is listed among the top 10 EduTech companies in India, now \n        acquired by BYJU's which is among the top EduTech companies in the world. \n        At Toppr, I managed end-to-end technology implementations for Toppr School \n        platform, a SaaS application given to schools to conduct online classes, \n        quizzes, and exams.",
+  pointerHeader: 'Achievements:',
+  pointers: ['Delivered the quiz platform in less than 21 days.', 'Successfully delivered and managed the end-to-end implementation of online classes via Zoom API.']
+}, {
+  organization: {
+    name: 'Xwards',
+    url: '#'
+  },
+  endTime: 'July 2020',
+  startTime: 'September 2018',
+  position: 'Co-Founder & CTO',
+  skills: ['Node.js', 'Fluentd', 'MongoDB', 'MySQL', 'Neo4J', 'React-Native'],
+  desciption: "Xwards was my second startup. It was an in-cab mobility advertisement company \n        based out of Bangalore. At Xwards, we installed tablets inside taxis for passengers to \n        watch content such as news, videos, live TV, etc. Based on the passengers watching \n        preferences an intelligent system would push advertisements to the tablet.",
+  pointerHeader: 'Major Technical Challenges:',
+  pointers: ['Managing no-buffering on content for a seamless experience.', 'Syncing logs and user activities while the tablet was in no network zone.', 'Real-time intelligence for pushing Ads based on logs from devices.', 'Developing systems to stop device theft.']
+}, {
+  organization: {
+    name: 'Tech Talks Certifications',
+    url: '#'
+  },
+  endTime: 'February 2019',
+  startTime: 'January 2017',
+  position: 'Founder & CEO',
+  skills: ['Node.js', 'AngularJs', 'MongoDB', 'MySQL', 'Neo4J'],
+  desciption: "Tech Talks Certification, formerly called Tech Talks India, was my first startup \n        in 2017. It took me 6 months to do market research and come up with a viable business model \n        and the first product. At Tech Talks we were trying to establish technology accreditation \n        body for certification of open-source technology."
+}, {
+  organization: {
+    name: 'Justdial Labs',
+    url: 'https://www.justdial.com/'
+  },
+  endTime: 'January 2017',
+  startTime: 'April 2016',
+  position: 'Sr. Software Engineer',
+  skills: ['Node.js', 'Fluentd', 'PHP', 'ElasticSearch']
+}, {
+  organization: {
+    name: 'Pegasystems',
+    url: 'https://www.pega.com/'
+  },
+  endTime: 'April 2016',
+  startTime: 'April 2015',
+  position: 'Software Engineer',
+  skills: ['AngularJs', 'Node.js', 'Pega7', 'JavaScript']
+}, {
+  organization: {
+    name: 'Tata Consultancy Services',
+    url: 'https://www.tcs.com/'
+  },
+  endTime: 'April 2015',
+  startTime: 'July 2013',
+  position: 'Software Engineer',
+  skills: ['AngularJs', 'Node.js', 'PHP', 'JavaScript', 'MySQL', 'HTML', 'CSS']
+}];
+exports.experiences = experiences;
+//# sourceMappingURL=experience.dev.js.map
--- page-components/profile/index.jsx
+++ page-components/profile/index.jsx
@@ -1,53 +1,25 @@
 import { Button } from 'react-bootstrap';
 import styles from '../../styles/profile.module.scss';
+import {experiences} from './data/experience';
+import Experience from './experience';
 
 export default function Profile() {
-
-
+    
+    const downloadResume = () => {
+        window.location.href = "https://veershrivastav.com/veer_shrivastav.pdf"
+    }
 
   	return (
 		<div className='container-flex relative page' id='profile'>
             <div className='row' id={styles.profile}>
                 <div className='col-12'>
-                    <Button id={styles.resumeBtn} size={'md'}>
+                    <Button id={styles.resumeBtn} onClick={downloadResume} size={'md'}>
                         <i className="bi bi-download"></i> Resume
                     </Button>
                     <h1 className='title'>Experience</h1>
-                    <div className={`relative ${styles.workEx}`}>
-                        <h5 className={styles.endTime}>March 2023</h5>
-                        <h2 className={styles.organization}><a href='https://damensch.com' target='_new'>DaMENSCH</a></h2>
-                        <h3 className={styles.position}>VP of Engineering</h3>
-                        <div className={styles.skillset}>
-                            <span className='code'>PHP</span>
-                            <span className='code'>mongoDB</span>
-                            <span className='code'>Java</span>
-                        </div>
-                        <div className={`d-flex ${styles.widthsm50Container}`}>
-                            <div className={`d-flex ${styles.widthsm50}`}>
-                                <h6 className={`subtitle`}>Description</h6>
-                                <p className={styles.description}>
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                                    when an unknown printer took a galley of type and scrambled it to make a
-                                </p>
-                            </div>
-                            <div className={`d-flex ${styles.widthsm50}`}>
-                                <h6 className={`subtitle`}>Key Responsibilities</h6>
-                                <ul>
-                                    <li>Lorem Lorem Lorem Lorem</li>
-                                    <li>Lorem Lorem Lorem Lorem</li>
-                                    <li>Lorem Lorem Lorem Lorem</li>
-                                </ul>  
-                            </div>
-                        </div>
-                        <h5 className={styles.startTime}>July 2021</h5>
-                    </div>
-                    <div className={`relative ${styles.workEx}`}>
-                        <h5 className={styles.endTime}>March 2023</h5>
-                        <h2 className={styles.organization}><a href='https://damensch.com' target='_new'>DaMENSCH</a></h2>
-                        <h3 className={styles.position}>VP of Engineering</h3>
-                        <h5 className={styles.startTime}>July 2021</h5>
-                    </div>
+                    {experiences.map(experience => {
+                        return (<Experience data={experience} />)
+                    })}
                 </div>
             </div>
 		</div>
